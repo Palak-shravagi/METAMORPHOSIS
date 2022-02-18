e5affb7671396cb2ed53b30864eac08d5efae54d
Palak-shravagi <palakshravagi@gmail.com>
2022-02-18 20:11:45 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Landing from "./Components/Landing.js"
+function App() {
+  return <div>
+    <Landing></Landing>
+  </div>;
+}
+
+export default App;
